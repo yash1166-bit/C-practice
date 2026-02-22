059a3961658bc6b98e7a27173436684f08481b33
Yashvanth-11 <yashrocky1166@gmail.com>
2026-02-22 13:38:16 +0530

    Added even or odd program using if-else

---
+++ even_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0) {
+        printf("Even number\n");
+    } else {
+        printf("Odd number\n");
+    }
+
+    return 0;
+}
